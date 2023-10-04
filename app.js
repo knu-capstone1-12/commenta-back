b3f6cf2e28668980b18c12709d4ce750c1c0fa56
risingstaruj <risingstaruj@naver.com>
2023-10-04 17:04:54 +0900

    Initial Settings and typescript set

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+//Express Load
+
+const app = express();
+//Express Application Definition
+
+app.get('/', (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.listen(4000, () => console.log("Waiting on 4000 port."));
