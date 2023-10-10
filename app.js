78b2c00b5147593aeaa7dbc9818773675d712be0
hankn01 <risingstaruj@gmail.com>
2023-10-11 01:52:22 +0900

    File Upload Feature Added

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 //Express Load
 
 const app = express();
@@ -6,6 +11,14 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello, World!");
+});
+
+app.post('/sttrec', upload.single('files'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('Please Upload file');
+    }
+
+    res.status(200).send('uploaded');
 });
 
 app.listen(4000, () => console.log("Waiting on 4000 port."));
