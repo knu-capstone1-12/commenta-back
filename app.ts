65ac43143e7243512c0fdff8d6ac38494102aa0a
hankn01 <risingstaruj@gmail.com>
2023-10-11 18:58:45 +0900

    Unused code removed

--- app.ts
+++ app.ts
@@ -3,14 +3,6 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
-const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, './uploads/'); // 파일이 저장되는 경로입니다.
-        },
-        filename: function (req, file, cb) {
-            cb(null, file.originalname); // 파일 저장 포멧
-        },
-});
 const upload = multer({storage: multer.diskStorage({
       	filename(req, file, done) {
           	console.log(file);
@@ -35,21 +27,7 @@ app.post('/sttrec', upload.single('audio'), (req, res) => {
     if(!req.file) {
         return res.status(400).send('Please Upload file');
     }
-
-    // 파일을 저장할 경로를 지정
-    // const filePath = path.join(__dirname, 'uploads', req.file.originalname);
-
-    // // 파일을 저장
-    // fs.writeFile(filePath, req.file.buffer, (err) => {
-    //     if (err) {
-    //         return res.status(500).send('File could not be saved');
-    //     }
-
-    //         res.status(200).send('uploaded');
-
-    // });
     res.status(200).send('uploaded');
-
 
 })
 
